8b96b8751d03e79942376a7b0d49a195c1ff93aa
Joseph Autorino <staircasesoul@gmail.com>
2021-04-05 22:00:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var iceCreams = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
